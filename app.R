a806799b01572d441fdf3e7e51aab75b34238b3f
Madhusha98 <121290679+Madhusha98@users.noreply.github.com>
2022-12-23 19:28:06 +0530

    Update app.R

--- app.R
+++ app.R
@@ -6,50 +6,116 @@ library(shinydashboard)
 
 ui = navbarPage(
   "Examination Timetable Dashboard", theme = shinytheme("slate"),
-  tabPanel("Overview",
-           h2("KPI Dashboard")
-  ),  
-   tabPanel("Exam Distribution",
+   tabPanel("Overview",
             sidebarLayout(
               sidebarPanel(
-                id = "sidebar",
-                width = 2,
-                # Input: Select the random distribution type ----
-                radioButtons("dist", "Distribution type:",
-                             c("Normal" = "norm",
-                             "Uniform" = "unif",
-                             "Log-normal" = "lnorm",
-                             "Exponential" = "exp")
-                             ),
-                             
-                # br() element to introduce extra vertical spacing ----
-                br(),
-                # Input: Slider for the number of observations to generate ----
-                sliderInput("n",
-                            "Number of observations:",
-                            value = 500,
-                            min = 1,
-                            max = 1000
-                            )
+                width = 3,
+                selectInput(
+                  inputId = "yr1",
+                  label = "Academic year",
+                  choices = c("First Year",
+                              "Second Year",
+                              "Third Year",
+                              "Fourth Year"), 
+                  selected = "All",
+                  multiple = FALSE
+                ),
+                conditionalPanel(
+                  condition = "input.yr1 =='Third Year'",
+                  selectInput(
+                    "dg.type1", "Degree type",
+                    c("General Degree",
+                      "Special Degree"))
+                ),
+                conditionalPanel(
+                  condition = "input.yr1 =='Fourth Year'",
+                  selectInput(
+                    "dg.type2", "Degree type",
+                    c("Special Degree",
+                      "Extended Degree"))
+                ),
+                selectInput(
+                  inputId = "sub1",
+                  label = "Subject(s)",
+                  choices = c("Chemistry",
+                              "Statistics"), 
+                  selected = "All",
+                  multiple = TRUE
+                )
                           ),
               mainPanel(
-                fluidRow(
-                  box(width = 12, height = 12,
-                    plotOutput("plot")
-                  )
-                )
+                
               )
             )
   ),
-  tabPanel("Location",
-           h2("Location Distribution"),
-           verbatimTextOutput("summary")
+  tabPanel("Exam distribution",
+           sidebarLayout(
+             sidebarPanel(
+               width = 3,
+               selectInput(
+                 inputId = "yr2",
+                 label = "Academic year",
+                 choices = c("First Year",
+                             "Second Year",
+                             "Third Year",
+                             "Fourth Year"), 
+                 selected = "All",
+                 multiple = FALSE
+               ),
+               conditionalPanel(
+                 condition = "input.yr2 =='Third Year'",
+                 selectInput(
+                   "dg.type3", "Degree type",
+                   c("General Degree",
+                     "Special Degree"))
+               ),
+               conditionalPanel(
+                 condition = "input.yr2 =='Fourth Year'",
+                 selectInput(
+                   "dg.type4", "Degree type",
+                   c("Special Degree",
+                     "Extended Degree"))
+               ),
+               selectInput(
+                 inputId = "sub2",
+                 label = "Subject(s)",
+                 choices = c("Chemistry",
+                             "Statistics"), 
+                 selected = "All",
+                 multiple = TRUE
+               ),
+               dateRangeInput('dateRange2',
+                              label = 'Date',
+                              start = Sys.Date(), end = Sys.Date() + 9,
+                              separator = "-", 
+                              weekstart = 1),
+               tags$style(HTML(".datepicker {z-index:99999 !important;}"))
+             ),
+             mainPanel(
+               
+             )
+           )
            ),
-  navbarMenu("More",
-             tabPanel("Summary", "Summary tab contents..."),
-             tabPanel("Table", "Table tab contents...")
+  tabPanel("Location",
+           sidebarLayout(
+             sidebarPanel(
+               width = 3,
+               selectInput(
+                 inputId = "location3",
+                 label = "Location(s)",
+                 choices = c("NFC 4",
+                             "Science Auditorium"), 
+                 selected = "All",
+                 multiple = TRUE
+               )
+             ),
+             mainPanel(
+               
+             )
+           )
   )
                 )
+
 
 server <- function(input, output) {
     
