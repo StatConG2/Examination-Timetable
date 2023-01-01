2fc5b74ba6a520a4cd9b84001214259f7b7adddc
Udeshi Salgado <udeshisalgado1998@gmail.com>
2023-01-01 14:53:00 +0530

    Adding end time which needs to plot locations distribution

--- Dataset.R
+++ Dataset.R
@@ -19,6 +19,7 @@ head(mainData)
 
 #Splitting course_code to extract subject, year and credits
 newData <- separate(mainData, unit_no, into = c("subject", "year", "credits"), sep = " ")
+
 head(newData)
 
 #extract 1st character from year and turn in to integer
@@ -33,8 +34,18 @@ newData$start_time <- format(as.numeric(str_sub(newData$time,1,5)), nsmall = 2)
 newData$start_time <- ifelse(as.integer(newData$start_time) >= 7 & as.integer(newData$start_time) < 12, paste(newData$start_time, "AM"), paste(newData$start_time, "PM"))
 newData$start_24hrs <- format(strptime(newData$start_time, format = "%I.%M %p"), format = '%H:%M')
 head(newData, 30)
-finalData <- subset(newData, select=c(1,2,9,10,3,4,8,6,5,7))
+
+
+# extract end time
+newData$end_time <- format(as.numeric(str_sub(newData$time,9,13)), nsmall = 2)
+newData$end_time <- ifelse(as.integer(newData$end_time) >= 7 & as.integer(newData$end_time) < 12, paste(newData$end_time, "AM"), paste(newData$end_time, "PM"))
+newData$end_24hrs <- format(strptime(newData$end_time, format = "%I.%M %p"), format = '%H:%M')
+head(newData, 30)
+
+
+finalData <- subset(newData, select=c(1,2,9,11,10,12,3,4,8,6,5,7))
 finalData$venue <- replace(finalData$venue, finalData$venue=="Statistics Computer La", "Statistics Computer Lab")
+#view(finalData)
 
 # exporting final dataset
 #library("writexl")
