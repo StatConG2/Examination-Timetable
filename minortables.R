bcd2c2ca4a4fc8cc8fceccbc877b09f8d0c1e80b
Madhusha <madhushaperera0898@gmail.com>
2023-01-12 11:43:17 +0530

    Changing table 1 and table 2

--- minortables.R
+++ minortables.R
@@ -5,9 +5,9 @@
 ### Year & Degree Types (Table 1)
 
 year <- c("First", "Second", "Third", "Third", "Fourth", "Fourth")
-degreetype <- c("General Degree", "General Degree", "General Degree", "Special Degree", "General Degree", "Extended Degree")
+degreetype <- c("General Degree", "General Degree", "General Degree", "Special Degree", "Special Degree", "Extended Degree")
 table1 <- data.frame(year, degreetype)
-print(table1)
+#print(table1)
 
 ### Department & Subject (Table 2)
 
@@ -17,8 +17,8 @@ department <- c(rep(c("MAT"), times = 3), rep(c("ZOO"), times = 2),
                 rep(c("FST"), times = 2), rep(c("BIO"), times = 3),
                 rep(c("PCH"), times = 2),
                 c("EMF", "GMB", "SSM", "STA"))
-                
-                
+
+
 subject <- c("MAT", "AMT", "MAN", "ARM", "ZOO", "CHE", "ICH", "CSC",
              "ICT", "ECN", "ASP", "ASB", "ASC", "PHY", "EES", "FST",
              "FSC", "BIO", "MBL", "PBL", "PCH", "PST", "EMF", "GMB", 
@@ -26,20 +26,23 @@ subject <- c("MAT", "AMT", "MAN", "ARM", "ZOO", "CHE", "ICH", "CSC",
 
 table2 <-  data.frame(department, subject)           
 
-print(table2)
+#print(table2)
 
 
 
 ### Stream & Subject (Table 3)
 
-stream <- c(rep(c("Physical"), times = 14), rep(c("Biology"), times = 15), c("Food Science and Technology"), c("Sports Science and Management"))
+stream <- c(rep(c("Physical"), times = 16), 
+            rep(c("Biology"), times = 16), 
+            rep("Food Science and Technology",times=2), 
+            rep("Sports Science and Management",times=2))
 
-subject <- c("MAT", "CHE", "PHY", "STA", "MAN", "CSC", "AMT",
-              "EES", "ICT", "EMF", "PST", "PSC", "ASP", "ASC", "CHE", "ZOO", "PHY",
-              "PBT", "PBL", "MBL", "EMF", "ARM", "MAN", "FSC", "BIO",
-              "GMB", "FSC", "ASB", "ASC", "FST", "SSM")                                     
+subject <- c("MAT", "CHE", "PHY", "STA", "MAN", "CSC", "AMT","ENG","ECN",
+             "EES", "ICT", "EMF", "PST", "PSC", "ASP", "ASC", "CHE", "ZOO", "PHY",
+             "PBT", "PBL", "MBL", "EMF", "ARM", "MAN", "FSC", "BIO","ENG",
+             "GMB", "FSC", "ASB", "ASC", "FST", "ENG","SSM","ENG")                                     
 
 table3 <- data.frame(stream, subject)
 
-print(table3)
-               
+#print(table3)
+
